36926f61179d71c9171516805d2ebef1a10dc1fb
Jamie Wiles <jamie.d.wiles@gmail.com>
2020-07-24 16:50:44 +0100

    Minor update
    Co-Authored-By: Will Howes <48259950+willhowes@users.noreply.github.com>

--- library-backend/src/main/java/library/LoanRepository.java
+++ library-backend/src/main/java/library/LoanRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface LoanRepository extends CrudRepository<Loan, Integer> {
 
-  @Query("from Loan l inner join Book b on l.bookOnLoan = b.id where b.isbn=:isbn and l.user=:userId and l.returned = false order by l.dueDate")
+  @Query("from Loan l inner join Book b on l.bookOnLoan = b where b.isbn=:isbn and l.user=:userId and l.returned = false order by l.dueDate")
   List<Loan> findActiveLoansBy(@Param("isbn")long isbn, @Param("userId")String userId);
-
 }
