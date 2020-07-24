c20521a0b67b4c53ef58bd42342eab4f6fbb3790
Rachel Griffiths <racheljgriffiths@googlemail.com>
2020-07-24 16:03:39 +0100

    create CatalogueItem & dummy catalogue entries
    Co-Authored-By: Jim Berry <62150078+jamboree97@users.noreply.github.com>
    Co-Authored-By: jammie-dodger <37250724+jammie-dodger@users.noreply.github.com>
    Co-Authored-By: Joseph Townshend <8555574+josephtownshend@users.noreply.github.com>
    Co-Authored-By: Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
    Co-Authored-By: Will Howes <48259950+willhowes@users.noreply.github.com>
    Co-Authored-By: Diptis Halder <44645682+dippedincode@users.noreply.github.com>

--- library-app/src/Containers/Catalogue/Catalogue.js
+++ library-app/src/Containers/Catalogue/Catalogue.js
@@ -3,21 +3,28 @@ import Header from "../../Components/Header";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 
 class Catalogue extends Component {
+  
+  state = {
+    books: [{ title: "Harry Potter 1", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" }, 
+            { title: "Harry Potter 2", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" },
+            { title: "Harry Potter 3", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" }]
+  }
+
   render() {
-  return (
-    <div>
-      <Header />
-        <SearchBar
-          // value={this.state.searchText}
-          // searchWords={this.state.URL}
-          // onTextInput={this.handleChangeSearchText}
-          // onSearch={this.handleDoSearch}
-        >
-          {this.props.children}
-        </SearchBar>
-        <p>Hello, this is the catalogue</p>
-    </div>
-  );
-};
+    return (
+      <div>
+        <Header />
+          <SearchBar
+            // value={this.state.searchText}
+            // searchWords={this.state.URL}
+            // onTextInput={this.handleChangeSearchText}
+            // onSearch={this.handleDoSearch}
+          >
+            {this.props.children}
+          </SearchBar>
+          <p>Hello, this is the catalogue</p>
+      </div>
+    );
+  };
 }
 export default Catalogue;
