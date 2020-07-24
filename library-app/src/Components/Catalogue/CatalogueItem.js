c797ae182cd91861a8488166fd96d51df643ac3c
Jim Berry <jim.berry@alfasystems.com>
2020-07-24 17:17:46 +0100

    CSS styling of catalogue
    Co-Authored-By: Will Howes <48259950+willhowes@users.noreply.github.com>
    Co-Authored-By: jammie-dodger <37250724+jammie-dodger@users.noreply.github.com>
    Co-Authored-By: Rachel Griffiths <racheljgriffiths@googlemail.com>
    Co-Authored-By: Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
    Co-Authored-By: Joseph Townshend <8555574+josephtownshend@users.noreply.github.com>
    Co-Authored-By: Diptis Halder <44645682+dippedincode@users.noreply.github.com>

--- library-app/src/Components/Catalogue/CatalogueItem.js
+++ library-app/src/Components/Catalogue/CatalogueItem.js
@@ -1,13 +1,18 @@
 import React from "react";
+import classes from "./CatalogueItem.module.css";
 
-const CatalogueItem = (props) => { 
-    return (
-        <div>
-            <p>
-                Hello I'm a catalogue item!
-            </p>
-        </div>
-    )
-}
+const CatalogueItem = (props) => {
+  return (
+    <div className={classes.Table}>
+      <div className={classes.TableEntryTitle}>{props.image}</div>
+      <div className={classes.TableEntryTitle}>{props.title}</div>
+      <div className={classes.TableEntryTitle}>{props.author}</div>
+      {/* <div className={classes.TableEntryDescription}>{props.description}</div> */}
+      <div className={classes.TableEntryButton} onClick={props.clicked}>
+        See more
+      </div>
+    </div>
+  );
+};
 
 export default CatalogueItem;
--- library-app/src/Containers/Catalogue/Catalogue.js
+++ library-app/src/Containers/Catalogue/Catalogue.js
@@ -1,43 +1,75 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from "../../Components/Header";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import CatalogueItem from "../../Components/Catalogue/CatalogueItem";
 
 class Catalogue extends Component {
-  
   state = {
-    books: [{ title: "Harry Potter 1", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" }, 
-            { title: "Harry Potter 2", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" },
-            { title: "Harry Potter 3", description: "The Philosopher's Stone", author: "JK Rowling", image: "image here" }]
-  }
+    books: [
+      {
+        title: "Harry Potter 1",
+        description: "The Philosopher's Stone",
+        author: "JK Rowling",
+        image: "image here",
+        ISBN: "FakeIsbn",
+      },
+      {
+        title: "Harry Potter 20000",
+        description: "Lots happens in this book but does it mess up the table?",
+        author: "Test Quite a long name",
+        image: "image here",
+        ISBN: "FakeIsbn",
+      },
+      {
+        title: "Harry Potter 300000000",
+        description: "The Philosopher's Stone",
+        author: "Hatsunu Miku",
+        image: "image here",
+        ISBN: "FakeIsbn",
+      },
+    ],
+  };
+
+  openBookDetailsHandler = (book) => {
+    console.log(this.props);
+    this.props.history.push({
+      pathname: "/bookDetails",
+      search: "?" + this.state.books[0].ISBN,
+    });
+  };
 
   render() {
     let tableEntries = null;
 
-    tableEntries = this.state.books.map( (book) => { 
+    tableEntries = this.state.books.map((book) => {
       return (
-        <CatalogueItem 
+        <CatalogueItem
+          key={book.title}
           image={book.image}
-          title={book.title} 
-          description={book.description} 
-          author={book.author} />
-      )
-    })
+          title={book.title}
+          description={book.description}
+          author={book.author}
+          clicked={() => {
+            this.openBookDetailsHandler(book.title);
+          }}
+        />
+      );
+    });
 
     return (
       <div>
         <Header />
-          <SearchBar
-            // value={this.state.searchText}
-            // searchWords={this.state.URL}
-            // onTextInput={this.handleChangeSearchText}
-            // onSearch={this.handleDoSearch}
-          >
-            {this.props.children}
-          </SearchBar>
-          {tableEntries}
+        <SearchBar
+        // value={this.state.searchText}
+        // searchWords={this.state.URL}
+        // onTextInput={this.handleChangeSearchText}
+        // onSearch={this.handleDoSearch}
+        >
+          {this.props.children}
+        </SearchBar>
+        {tableEntries}
       </div>
     );
-  };
+  }
 }
 export default Catalogue;
