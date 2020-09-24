862582ebcb1f53087490f5210720d85e333152db
Jim Berry <jim.berry@alfasystems.com>
2020-09-24 14:33:30 +0100

    catalogue table

--- library-app/src/Pages/CatalogueScreen.js
+++ library-app/src/Pages/CatalogueScreen.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Catalogue from "../Containers/Catalogue/Catalogue";
 import SearchBar from "../Components/SearchBar/SearchBar";
 import LibraryNavbar from "../Components/Navbar/LibraryNavbar";
+import CatalogueTable from "../Containers/CatalogueTable/CatalogueTable";
 
 const CatalogueScreen = () => {
   return (
     <div>
       <LibraryNavbar />
-      <Catalogue />
+      <CatalogueTable />
+      {/* <Catalogue /> */}
     </div>
   );
 };
