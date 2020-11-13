f6197b160bf812790059939facb46029dfd7cce7
Jim Berry <jim.berry@alfasystems.com>
2020-11-13 15:02:21 +0000

    Adding search bar drop down feature
    Currently semi complete

--- library-app/src/Components/SearchBar/SearchBar.js
+++ library-app/src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,29 @@ import classes from "./SearchBar.module.css";
 import { Form, Button, FormControl } from "react-bootstrap";
 import axios from "axios";
 import {Redirect} from "react-router-dom";
+import DropdownMenu from "../../UI/Dropdown/DropdownMenu";
 
 const SearchBar = () => {
 
   const [searchParam, setSearchParam] = useState("");
+  const [value, setValue] = useState("");
   const [redirectLink, setRedirectLink] = useState("");
+
+
+const inputChangedHandler = (newVal) => {
+  console.log("input changed handler invoked");
+  setSearchParam(newVal);
+
+}
+
+const valueChangedHandler = (newVal) => {
+  console.log("value changed handler invoked")
+
+const redirectString = "/books?" + newVal;
+setRedirectLink(redirectString)
+}
+
+
 
   let redirectComponent = null;
 
@@ -24,7 +42,8 @@ const SearchBar = () => {
   return (
   <Form inline>
   {redirectComponent} 
-    <FormControl type="text" placeholder="Search" className="mr-sm-2" value={searchParam} onChange={(e) => setSearchParam(e.target.value)}/>
+  <DropdownMenu value={searchParam} valueChanged={(newVal) => valueChangedHandler(newVal)} inputChanged={(newVal) => inputChangedHandler(newVal)}/>
+    {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" value={searchParam} onChange={(e) => setSearchParam(e.target.value)}/> */}
     <Button variant="outline-primary" onClick={searchClickedHandler}>Search</Button>
   </Form>
   )
---
+++ library-app/src/UI/Dropdown/DropdownMenu.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const options = ['Example 1', 'Example 2', "example 3", "Different 1", "Different 2", "DIFFERENT CAPS"];
+
+export default function ControllableStates(props) {
+
+  return (
+    <div>
+      <Autocomplete
+        value={props.value}
+        onChange={(event, newValue) => {
+            props.valueChanged(newValue);
+          }}
+        inputValue={props.inputValue}
+        onInputChange={(event, newInputValue) => {
+            props.inputChanged(newInputValue);
+          }}
+        id="controllable-states-demo"
+        options={options}
+        style={{ width: 300, backgroundColor: "white", borderRadius: "5px", marginRight: "20px" }}
+        renderInput={(params) => <TextField {...params} label="Search"  placeholder="Search"/>}
+
+      />
+    </div>
+  );
+}
