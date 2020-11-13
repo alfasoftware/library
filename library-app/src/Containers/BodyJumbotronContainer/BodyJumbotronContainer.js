08809a832a5a2a86b11031474813c87528ece5e2
Jim Berry <jim.berry@alfasystems.com>
2020-11-13 09:02:37 +0000

    Adding onClick redirects to the Homepage

--- library-app/src/Containers/BodyJumbotronContainer/BodyJumbotronContainer.js
+++ library-app/src/Containers/BodyJumbotronContainer/BodyJumbotronContainer.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import jumboData from "./bodyjumbodata.json";
 import BodyJumbotron from "../../Components/BodyJumbotron/index";
 import { Redirect } from "react-router-dom";
 
 const BodyJumbotronContainer = () => {
+
+  const [redirect, setRedirect] = useState(null);
+
+  let redirectComponent = null;
+
+  if(redirect) {
+    redirectComponent = <Redirect to={redirect} />
+  }
+
   return (
     <BodyJumbotron.Container>
+      {redirectComponent}
       {jumboData.map((item) => (
-        <BodyJumbotron key={item.id} direction={item.direction}>
+        <BodyJumbotron key={item.id} direction={item.direction} onClick={() => setRedirect(item.redirectLink)}>
           <BodyJumbotron.Pane>
             <BodyJumbotron.Title>{item.title}</BodyJumbotron.Title>
             <BodyJumbotron.SubTitle>{item.subTitle}</BodyJumbotron.SubTitle>
